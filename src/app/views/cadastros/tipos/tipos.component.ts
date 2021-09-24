ccc1fce43bd45e88e8019ccb19b0900a8bca1fb7
Lucas Vilas Boas <lucasvilasboaslage@gmail.com>
2021-09-24 20:56:06 -0300

    Criando dockerfile Adicionando página de tipo despesa Adicionando modal

--- src/app/views/cadastros/tipos/tipos.component.ts
+++ src/app/views/cadastros/tipos/tipos.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ModalDirective } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-tipos',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TiposComponent implements OnInit {
 
+  @ViewChild('myModal') public myModal: ModalDirective;
+  
   constructor() { }
 
   ngOnInit(): void {
